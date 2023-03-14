cdb1d32d6b2f41f3d59f7ad68a216cbc3b8fe0c0
washesss <washington.rogati@etec.sp.gov.br>
2023-03-15 05:34:15 +0900

    Adição das tabelas

--- www/components/motor.js
+++ www/components/motor.js
@@ -5,6 +5,29 @@ function calcularconsumo(){
  var marca = document.getElementById('marca').value;
  var modelo = document.getElementById('modelo').value;
 
- document.getElementById("consumo").innerHTML = "Seu " + marca + " " + modelo + " fez " + km / gas + "KM/L" ;
+ var consumo = km / gas;
 
+ document.getElementById("consumo").innerHTML = "Seu " + marca + " " + modelo + " fez " + consumo + "KM/L" ;
+ var paragraph2 = document.getElementById("paragraph2");
+ var paragraph1 = document.getElementById("paragraph1");
+ var paragraph3 = document.getElementById("paragraph3");
+ var paragraph4 = document.getElementById("paragraph4");
+  if(consumo >= 20){
+  paragraph1.style.backgroundColor = "red";
+  }
+
+  else if( consumo >= 12 && consumo < 20){
+  paragraph2.style.backgroundColor = "red";
+  }
+  
+  else if( consumo >= 8 && consumo < 12){
+  paragraph3.style.backgroundColor = "red";
+  }
+  
+  else if (consumo <= 8) {
+  paragraph4.style.backgroundColor = "red";
+  }
+  else{
+      alert("NAO TEM")
+  }
 }
